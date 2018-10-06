705eefbd586c330e5843abcfa57a254176248057
Anas YAHYAOUI <anyahyao@e2r6p3.42.fr>
2018-10-06 14:56:24 +0200

    lem_in.h

--- lem_in.h
+++ lem_in.h
@@ -6,7 +6,7 @@
 /*   By: anyahyao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/30 19:00:27 by anyahyao          #+#    #+#             */
-/*   Updated: 2018/09/30 21:01:08 by anyahyao         ###   ########.fr       */
+/*   Updated: 2018/10/03 21:44:44 by anyahyao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,53 @@
 #define __LEM_IN__
 #include <stdio.h>
 
+#define WHITE 0
+#define GREY 1
+#define BLACK 2
 #include "libft/includes/libft.h"
+#include "libft/includes/ft_printf.h"
+
+typedef struct				s_path
+{
+	unsigned long long int	*path;
+	int						next_node;  
+	unsigned int			nb_path;
+	int						nb_next_node;
+}							t_path;
 
 typedef struct		s_chemins
 {
 	unsigned char	*path;
 	unsigned char	**all_path;
 	unsigned int	nb_path;
+	unsigned int	nb_max;
 }					t_chemins;
 
 typedef struct		s_graphe
 {
-	int			taille;
-	int			**map;
-}				t_graphe;
+	int				taille;
+	int				**map;
+	int				*color;
+	int				end;
+}					t_graphe;
 
-typedef struct	s_big_tab
+typedef struct		s_big_tab
 {
-	int			taille;
-	int			*tab;
-	int			*t1;
-	int			*t2;
-}				t_big_tab;
+	int				taille;
+	int				*tab;
+	int				*t1;
+	int				*t2;
+}					t_big_tab;
 
-void	free_graphe(t_graphe *g);
-void			ft_mem_set_int(int *tab, int val, int taille);
-int		*create_tab(int taille, int val);
-void	tab_swap(int **a, int **b);
-int		**create_double_tab(int taille, int val);
-t_big_tab		*new_big_tab(t_graphe *g);
-int				short_path(t_graphe *g, t_big_tab *bt, int begin, int end);
-void			reboot_big_tab(t_big_tab *bt, int begin);
-int		ajout_voisin(t_graphe *g, t_big_tab *bt, int k, int node);
+void				test_init_all_path(t_graphe *g, int end);
+void				free_graphe(t_graphe *g);
+void				ft_mem_set_int(int *tab, int val, int taille);
+int					*create_tab(int taille, int val);
+void				tab_swap(int **a, int **b);
+int					**create_double_tab(int taille, int val);
+t_big_tab			*new_big_tab(t_graphe *g);
+int					short_path(t_graphe *g, t_big_tab *bt, int begin, int end);
+void				reboot_big_tab(t_big_tab *bt, int begin);
+int					ajout_voisin(t_graphe *g, t_big_tab *bt, int k, int node);
 
 #endif
